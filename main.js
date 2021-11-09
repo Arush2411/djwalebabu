d8c284c176195dee21fc6a32edf7be2559886a72
Arush2411 <86655897+Arush2411@users.noreply.github.com>
2021-11-09 14:44:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 song1 = "";
 song2 = "";
 
+song1_status = "";
+song2_status = "";
 
 scoreRightWrist = 0;
 scoreLeftWrist = 0;
@@ -54,7 +56,36 @@ function gotPoses(results)
 
 function draw() {
 	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+		}
 	}
+
+}
+
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
 
 
 
